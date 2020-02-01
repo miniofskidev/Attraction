373a3c8670eb2e0038b93bca7cc8af9bfc049cf0
Amir Ardroudi <a_ardroudi@live.com>
2020-02-01 22:15:01 +0330

    enemy patrol script + enemy prefab

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    [Range(0, 10)]
+    public float distance = 0;
+    [Tooltip("it will move vertical by default")]
+    public bool moveHorizantal;
+    private Vector2 startPoint;
+    private Vector2 destPoint;
+
+    void Start()
+    {
+        startPoint = transform.position;
+        destPoint = new Vector2(transform.position.x + distance, transform.position.y + distance);
+    }
+
+    void Update()
+    {
+        if (moveHorizantal)
+        {
+            transform.position = new Vector3(
+                Mathf.PingPong(Time.time * 2, destPoint.x - startPoint.x) + startPoint.x,
+                transform.position.y,
+                transform.position.z
+                );
+        }
+        else
+        {
+            transform.position = new Vector3(
+                transform.position.x,
+                Mathf.PingPong(Time.time * 2, destPoint.y - startPoint.y) + startPoint.y,
+                transform.position.z
+                );
+        }
+    }
+}
