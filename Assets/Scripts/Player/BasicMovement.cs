65f95781c18cc8850eea76eb7692f074a26f9e87
Amin Ahrabian <aahrabian@gmail.com>
2020-02-01 17:33:35 +0100

    Initial Commit

---
+++ Assets/Scripts/Player/BasicMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour {
+
+    private float movementSpeed = 5f;
+
+    void Update() {
+        
+      Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);  
+
+      transform.position = transform.position + movement * Time.deltaTime * movementSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+      
+    }
+}
