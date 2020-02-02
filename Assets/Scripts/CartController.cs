807b09ba368bc6373c3656e3fab247ebf06f8510
Amin Ahrabian <aahrabian@gmail.com>
2020-02-02 07:06:07 +0100

    Player Rail movement basics

--- Assets/Scripts/CartController.cs
+++ Assets/Scripts/CartController.cs
@@ -22,21 +22,17 @@ public class CartController : MonoBehaviour {
     targetPosX = target.x;
 
     if (currentPosX <= targetPosX && Mathf.Approximately(currentPosY , targetPosY)) {      
-      Debug.Log("Moving Right");
 
       animator.SetInteger("Direction", 0);
 
     } else if (currentPosX >= targetPosX && Mathf.Approximately(currentPosY, targetPosY)) {
-      Debug.Log("Moving Left");
 
       animator.SetInteger("Direction", 1);
     }
 
     if (currentPosY < targetPosY && Mathf.Approximately(currentPosX, targetPosX)) {
-      Debug.Log("Moving Up");
 
     } else if (currentPosY > targetPosY && Mathf.Approximately(currentPosX, targetPosX)) {
-      Debug.Log("Moving Down");
 
       animator.SetInteger("Direction", 2);
     }
--- Assets/Scripts/Player/BasicMovement.cs
+++ Assets/Scripts/Player/BasicMovement.cs
@@ -7,32 +7,79 @@ public class BasicMovement : MonoBehaviour {
     public Rigidbody2D rigidbody;
     public Animator animator;  
 
+    public GameObject railVertical;
+    public GameObject railHorizontal;
+
     private Vector2 movement;
 
     private float movementSpeed = 5f;
     private float directionMultiplier;
 
-    private void Update() {
+    // 0 is nothing, 1 is horzontal, 2 is vertical
+    public int carriageStat = 0;
+
+    void Update() {
+
       movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));  
+
+      if (Input.GetButtonDown("FixTheDamnThing")) {
+        placeItem();
+      }
     }
 
     void FixedUpdate() {
             
       animator.SetFloat("Horizontal", Mathf.Abs(movement.x));
-      animator.SetFloat("Vertical", movement.y);
-      //animator.SetFloat("Magnitude", movement.y);
+      animator.SetFloat("Vertical", movement.y);      
 
       directionMultiplier = movement.x < 0 ? -2 : 2;
 
       transform.localScale = new Vector2(directionMultiplier, transform.localScale.y);
 
-      rigidbody.MovePosition(rigidbody.position + movement * movementSpeed * Time.fixedDeltaTime);
+      rigidbody.MovePosition(rigidbody.position + movement * movementSpeed * Time.fixedDeltaTime);      
+    }
+
+    private void placeItem() {
+
+      Debug.Log("Place Item");
+
+      if (carriageStat == 1) {
+        
+        Instantiate(railHorizontal, transform.position, transform.rotation);
+      } else if (carriageStat == 2) {
+
+        Instantiate(railVertical, transform.position, transform.rotation);
+      }    
+
+      carriageStat = 0;
+    }
+
+    private void pickUpItem(Collider2D other) {
+
+      Debug.Log("Picking up " + other.name + "  " + other.tag);
+
+      if (Input.GetButtonDown("PickTheDamnThing")) {
+
+        if (carriageStat == 0) {
+                
+          if (other.tag == "RailVertical") {            
+            carriageStat = 2;
+          } else if (other.tag == "RailHorizontal") {
+            carriageStat = 1;
+          }
+
+          other.gameObject.GetComponent<Rail>().shouldDestroy = true;
+        }
+      }
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+      
+      pickUpItem(other);
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
 
-        Debug.Log("Triggered with " + other.name + "  " + other.GetComponent<GameObject>().Tag;);
-
-        
+      pickUpItem(other);
     }
 }
