32f940ed7af8b988b7c3eacf2206e9b614dd9655
Amin Ahrabian <aahrabian@gmail.com>
2020-02-02 07:18:08 +0100

    More stuff

---
+++ Assets/Scripts/Rail.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rail : MonoBehaviour
+{
+
+    public bool shouldDestroy = false;
+
+    private void FixedUpdate() {
+
+      if (shouldDestroy) {
+        Destroy(this.gameObject);
+      }
+    }
+}
---
+++ Assets/Scripts/RailParentSensor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailParentSensor : MonoBehaviour
+{
+
+    private GameObject player;
+
+    private void Start() {
+      player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+
+      Debug.Log("OnTrigger from Sensor " + other.name);
+
+      if (other.tag == "Player") {
+        Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+      }
+
+      if (other.tag == "RailVertical" || other.tag == "RailHorizontal") {
+
+        Debug.Log("Rail placed");
+      }
+    }
+}
