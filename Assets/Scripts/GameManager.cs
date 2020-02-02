a570671a60a7724dc82974219d596fa5ed0bee8b
Amin Ahrabian <aahrabian@gmail.com>
2020-02-02 08:35:53 +0100

    Add Michele work to project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Game States
+// for now we are only using these two
+public enum GameState { MAIN_MENU, GAME }
+
+public delegate void OnStateChangeHandler();
+
+public class GameManager
+{
+    protected GameManager() { }
+    private static GameManager instance = null;
+    public event OnStateChangeHandler OnStateChange;
+    public GameState gameState { get; private set; }
+    public static int actualLevel = -1;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (GameManager.instance == null)
+            {
+                GameManager.instance = new GameManager();
+            }
+            return GameManager.instance;
+        }
+
+    }
+    
+    public void NextLevel()
+    {
+        Application.LoadLevel("level" + (++(actualLevel)));
+    }
+
+    public void SetGameState(GameState state)
+    {
+        this.gameState = state;
+        OnStateChange();
+    }
+
+    public void OnApplicationQuit()
+    {
+        GameManager.instance = null;
+    }
+
+}
---
+++ Assets/Scripts/MainPanel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPanel : MonoBehaviour
+{
+
+    public GameObject MenuPanel;
+    public GameObject CreditsPanel;
+    
+
+    GameManager GM;
+
+    private void Awake()
+    {
+        GM = GameManager.Instance;
+        GM.OnStateChange += HandleOnStageChange;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMenu();
+    }
+
+    public void ShowCredits()
+    {
+        MenuPanel.SetActive(false);
+        CreditsPanel.SetActive(true);
+    }
+
+    public void ShowMenu()
+    {
+        MenuPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+    }
+
+    private void HandleOnStageChange()
+    {
+        GM.SetGameState(GameState.MAIN_MENU);
+        Debug.Log("Sei su " + GM.gameState);
+    }
+
+    public void StartGame()
+    {
+        //GM.SetGameState(GameState.GAME);
+        Application.LoadLevel("TestLevel");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
