156e6a973dc2d39a4cc0aa29ffc604bd0da3ee4c
Amin Ahrabian <aahrabian@gmail.com>
2020-02-02 04:10:02 +0100

    Fix the Colliders on Player and Cart

---
+++ Assets/Scripts/CartController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartController : MonoBehaviour { 
+
+  public Animator animator;
+
+  public void updateAnimatorValues(Vector3 currentPos, Vector3 target) {
+    
+    // Set Direction value on animator left:1, right:2, down:3
+
+    float currentPosY = currentPos.y;
+    float currentPosX = currentPos.x;
+
+    float targetPosY = target.y;
+    float targetPosX = target.x;
+
+    if (currentPosX <= targetPosX && Mathf.Approximately(currentPosY , targetPosY)) {      
+      Debug.Log("Moving Right");
+
+      animator.SetInteger("Direction", 0);
+
+    } else if (currentPosX >= targetPosX && Mathf.Approximately(currentPosY, targetPosY)) {
+      Debug.Log("Moving Left");
+
+      animator.SetInteger("Direction", 1);
+    }
+
+    if (currentPosY < targetPosY && Mathf.Approximately(currentPosX, targetPosX)) {
+      Debug.Log("Moving Up");
+
+    } else if (currentPosY > targetPosY && Mathf.Approximately(currentPosX, targetPosX)) {
+      Debug.Log("Moving Down");
+
+      animator.SetInteger("Direction", 2);
+    }
+  }
+}
---
+++ Assets/Scripts/Node/PathFinder.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+
+    public GameObject cart;
+    private CartController  cartController;
+
+    Node[] nodes;
+  
+    public float speed = 4f;
+    int nodeIndex = 0;
+    float timer;
+
+    static Vector2 currentPosHolder;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+        nodes = GetComponentsInChildren<Node>();
+        
+        cartController = cart.GetComponent<CartController>();
+
+        checkNode();
+    }
+
+    void checkNode() {
+
+      timer = 0;
+
+      currentPosHolder = nodes[nodeIndex].transform.position;
+      
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+        timer = Time.deltaTime * speed;
+
+        Debug.Log("Update Call");
+
+        cartController.updateAnimatorValues(cart.transform.position, currentPosHolder);   
+
+        if (new Vector2(cart.transform.position.x, cart.transform.position.y) != currentPosHolder) {
+
+          // I need to move the player using rigidBody move method
+
+          cart.transform.position = Vector3.Lerp(cart.transform.position, currentPosHolder, timer);
+                                       
+        } else {
+          
+          if (nodeIndex < nodes.Length - 1) {
+            
+            nodeIndex++;
+            checkNode();
+          }
+        }
+    }
+}
